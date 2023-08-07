a49d27709741a117b63d9a3d5926d93be0b58381
OussamaDjebbour <oussama_djebbour@outlook.com>
2023-08-07 18:46:44 +0100

    version 1

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
 "use strict";
-const btnSubmit = document.querySelector("#submit");
-const btnRateAgain = document.querySelector("#rate-again");
-const btnsRate = document.querySelectorAll(".btn");
-const parentbtns = document.querySelector(".parent-number");
-const divThank = document.querySelector(".thank");
-const containerHidden = document.querySelector(".container1");
+const app = function () {
+  const btnSubmit = document.querySelector("#submit");
+  const btnRateAgain = document.querySelector("#rate-again");
+  const btnsRate = document.querySelectorAll(".btn");
+  const divThank = document.querySelector(".thank");
+  const containerHidden = document.querySelector(".container1");
+  let rate = "";
 
-const initialBtnColors = function () {
-  btnsRate.forEach((btn) => {
-    btn.style.backgroundColor = "hsl(216, 12%, 54%, 0.5)";
+  const initialBtnColors = function () {
+    // btnsRate.forEach((el) => (el.checked = false));
+    btnsRate[btnsRate.length - 1].checked = true;
+    rate = "5";
+  };
+
+  btnsRate.forEach((el) => {
+    el.addEventListener("change", function (e) {
+      rate = e.target.value;
+      console.log(rate);
+    });
+  });
+
+  const changeVisibilityContainer = function () {
+    btnSubmit.closest(".container").classList.toggle("hidden");
+    containerHidden.classList.toggle("hidden");
+  };
+
+  btnSubmit.addEventListener("click", function () {
+    divThank.textContent = `You selected out ${rate}  of  5 Thank you!`;
+    changeVisibilityContainer();
+  });
+
+  btnRateAgain.addEventListener("click", function () {
+    initialBtnColors();
+    changeVisibilityContainer();
   });
 };
 
-const changeVisibilityContainer = function () {
-  btnSubmit.closest(".container").classList.toggle("hidden");
-  containerHidden.classList.toggle("hidden");
-};
-
-parentbtns.addEventListener("click", function (e) {
-  const btnRateTarget = e.target.closest(".btn");
-  initialBtnColors();
-  if (!btnRateTarget) return;
-  btnRateTarget.style.backgroundColor = "hsl(25, 97%, 53%)";
-  btnSubmit.dataset.id = btnRateTarget.textContent;
-});
-
-btnSubmit.addEventListener("click", function (e) {
-  const rateNumber = e.target.dataset.id;
-  divThank.textContent = `You selected out ${rateNumber}  of  5 Thank you!`;
-  changeVisibilityContainer();
-});
-
-btnRateAgain.addEventListener("click", function () {
-  initialBtnColors();
-  changeVisibilityContainer();
-});
+app();
